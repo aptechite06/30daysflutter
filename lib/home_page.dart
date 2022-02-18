e002491c2278f1d9198027a024b1b50b314bed03
Umair Afzal <aptechite06@gmail.com>
2022-02-18 19:55:32 +0500

    First Commit

---
+++ lib/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Welcome to Flutter for $days Days by Umair'),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
